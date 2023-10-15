22d43854fe74e657d00d03650b541524293940c8
923048992@qq.com <923048992@qq.com>
2023-10-15 09:16:52 +0800

    log data

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from copy import deepcopy
+import json
 import os
 import random
 import numpy as np
 import torch
+from algorithmn.base import FedClientBase
 from algorithmn.fedavg import FedAvgClient, FedAvgServer
 from algorithmn.fedl2reg import FedL2RegClient, FedL2RegServer
 from algorithmn.fedper import FedPerClient, FedPerServer
@@ -61,6 +63,11 @@ if __name__ == '__main__':
     tensorboard_path = os.path.join('./tensorboard', sub_dir_name)
     writer = SummaryWriter(log_dir=tensorboard_path)
 
+    # setup training data dir
+    training_data_dir = os.path.join('./training_data', sub_dir_name)
+    weights_dir = os.path.join(tensorboard_path, 'weights')
+    training_data_json = os.path.join(training_data_dir, 'data.json')
+
     if train_rule not in FL_CLIENT or train_rule not in FL_SERVER:
         raise NotImplementedError()
 
@@ -79,6 +86,12 @@ if __name__ == '__main__':
         sample_size = int(args.model_het_percent * args.num_clients)
         heterogeneous_clients = random.sample(client_idxs, sample_size)
         print('heterogeneous clients:', heterogeneous_clients)
+
+    training_data = {
+        'heterogeneous clients': heterogeneous_clients
+    }
+    with open(training_data_json, 'w') as f:
+        f.write(json.dumps(training_data))
 
     for idx in client_idxs:
         local_model = deepcopy(global_model) if idx not in heterogeneous_clients \
@@ -111,3 +124,10 @@ if __name__ == '__main__':
             f"Local Accuracy on Local Data: {local_acc1:.2f}%, {local_acc2:.2f}%")
         local_accs1.append(local_acc1)
         local_accs2.append(local_acc2)
+
+        # save client weights every 5 epochs
+        if round % 5 == 0:
+            for idx in range(args.num_clients):
+                weights_path = os.path.join(weights_dir, f'client_{idx}.pth')
+                local_client: FedClientBase = local_clients[idx]
+                torch.save(local_client.local_model.state_dict())
