81ff5b2817dc53862985a971695fdabd85517e41
923048992@qq.com <923048992@qq.com>
2023-10-25 21:19:04 +0800

    fix

--- draw_pzy.py
+++ draw_pzy.py
@@ -7,7 +7,6 @@ from datasets import PACS
 
 from models.resnet import PACSResNet
 from torch.utils.data import DataLoader
-from sklearn.decomposition import PCA
 
 import matplotlib.pyplot as plt
 
@@ -27,19 +26,17 @@ if __name__ == "__main__":
     if dataset == "pacs":
         model = PACSResNet(num_classes=7, probabilistic=True).to(device)
         state_dict = torch.load(weights_path, map_location=device)
-        r_mu = state_dict["r.mu"]
-        r_sigma = state_dict["r.sigma"]
-        r_C = state_dict["r.C"]
+        r_mus = state_dict["r.mu"].to(device)
+        r_sigmas = state_dict["r.sigma"].to(device)
+        r_C = state_dict["r.C"].to(device)
         state_dict.pop("r.mu")
         state_dict.pop("r.sigma")
         state_dict.pop("r.C")
 
         model.load_state_dict(state_dict)
         dataset = PACS(root="./data", test_envs=[0])
-        label_dist = {i: {"mu": [], "sigma": []} for i in range(7)}
+        label_dist = {i: {"mu": [], "sigma": [], "kl_div": 0} for i in range(7)}
 
-        pca = PCA(n_components=1)
-        pca.fit_transform(data)
         for env in range(len(dataset.ENVIRONMENTS)):
             this_dataset = dataset[env]
             dataloader = DataLoader(dataset=this_dataset, batch_size=batch_size)
@@ -47,20 +44,34 @@ if __name__ == "__main__":
                 images, labels = images.to(device), labels.to(device)
                 _, output, (z_mu, z_sigma) = model(images, return_dist=True)
                 predict = torch.argmax(output, dim=1)
+                r_sigma = r_sigmas[labels]
+                r_mu = r_mus[labels]
                 for i in range(len(predict)):
                     p = predict[i].item()
-                    z_mu *= r_C
-                    z_sigma *= r_C
-                    label_dist[p]["mu"].extend(z_mu.mean(dim=1).tolist())
-                    label_dist[p]["sigma"].extend(z_sigma.mean(dim=1).tolist())
+                    this_z_mu = z_mu[i] * r_C
+                    this_z_sigma = z_sigma[i] * r_C
+                    label_dist[p]["mu"].append(this_z_mu.mean().item())
+                    label_dist[p]["sigma"].append(this_z_sigma.mean().item())
+                    label_dist[p]["kl_div"] += (
+                        torch.log(r_sigma[i])
+                        - torch.log(this_z_sigma)
+                        + (this_z_sigma**2 + (this_z_mu - r_mu[i]) ** 2)
+                        / (2 * r_sigma[i]**2)
+                        - 0.5
+                    ).sum().item()
+                break
+            break
 
         classes = ["dog", "elephant", "giraffe", "guitar", "horse", "house", "person"]
+        # print(label_dist)
         plt.xlabel("$\mu$")
         plt.ylabel("$\sigma$")
         for label in label_dist:
             dist = label_dist[label]
             mus = np.array(dist["mu"])
             sigmas = np.array(dist["sigma"])
+            kl_div = dist["kl_div"]
+
             total = len(mus)
             if total > 500:
                 sampled = random.sample(list(range(total)), 500)
