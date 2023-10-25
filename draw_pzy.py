b4271c17aa27e017e105b0932cb9d4f53d3e6ad5
923048992@qq.com <923048992@qq.com>
2023-10-25 21:38:50 +0800

    fix

--- draw_pzy.py
+++ draw_pzy.py
@@ -7,7 +7,7 @@ from datasets import PACS
 
 from models.resnet import PACSResNet
 from torch.utils.data import DataLoader
-
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
@@ -32,52 +32,63 @@ if __name__ == "__main__":
         state_dict.pop("r.mu")
         state_dict.pop("r.sigma")
         state_dict.pop("r.C")
-
         model.load_state_dict(state_dict)
         dataset = PACS(root="./data", test_envs=[0])
-        label_dist = {i: {"mu": [], "sigma": [], "kl_div": 0} for i in range(7)}
+        label_dist = {
+            i: {"mu": [], "sigma": [], "kl_div": 0, "z": []}
+            for i in range(7)
+        }
 
         for env in range(len(dataset.ENVIRONMENTS)):
             this_dataset = dataset[env]
             dataloader = DataLoader(dataset=this_dataset, batch_size=batch_size)
             for images, labels in dataloader:
                 images, labels = images.to(device), labels.to(device)
-                _, output, (z_mu, z_sigma) = model(images, return_dist=True)
+                z, output, (z_mu, z_sigma) = model(images, return_dist=True)
                 predict = torch.argmax(output, dim=1)
                 r_sigma = r_sigmas[labels]
                 r_mu = r_mus[labels]
                 for i in range(len(predict)):
                     p = predict[i].item()
-                    this_z_mu = z_mu[i] * r_C
-                    this_z_sigma = z_sigma[i] * r_C
-                    label_dist[p]["mu"].append(this_z_mu.mean().item())
-                    label_dist[p]["sigma"].append(this_z_sigma.mean().item())
-                    label_dist[p]["kl_div"] += (
-                        torch.log(r_sigma[i])
-                        - torch.log(this_z_sigma)
-                        + (this_z_sigma**2 + (this_z_mu - r_mu[i]) ** 2)
-                        / (2 * r_sigma[i]**2)
-                        - 0.5
-                    ).sum().item()
-                break
-            break
+                    label_dist[p]["z"].append(z[i].cpu().detach().tolist())
+                    # this_z_mu = z_mu[i] * r_C
+                    # this_z_sigma = z_sigma[i] * r_C
+                    # label_dist[p]["mu"].append(this_z_mu.mean().item())
+                    # label_dist[p]["sigma"].append(this_z_sigma.mean().item())
+                    # label_dist[p]["kl_div"] += (
+                    #     (
+                    #         torch.log(r_sigma[i])
+                    #         - torch.log(this_z_sigma)
+                    #         + (this_z_sigma**2 + (this_z_mu - r_mu[i]) ** 2)
+                    #         / (2 * r_sigma[i] ** 2)
+                    #         - 0.5
+                    #     )
+                    #     .sum()
+                    #     .item()
+                    # )
 
         classes = ["dog", "elephant", "giraffe", "guitar", "horse", "house", "person"]
         # print(label_dist)
         plt.xlabel("$\mu$")
         plt.ylabel("$\sigma$")
+        fig = plt.figure(figsize=(8, 6))
+        ax = fig.add_subplot(111, projection="3d")
+
         for label in label_dist:
             dist = label_dist[label]
-            mus = np.array(dist["mu"])
-            sigmas = np.array(dist["sigma"])
-            kl_div = dist["kl_div"]
+            # mus = np.array(dist["mu"])
+            # sigmas = np.array(dist["sigma"])
 
-            total = len(mus)
-            if total > 500:
-                sampled = random.sample(list(range(total)), 500)
-            else:
-                sampled = list(range(total))
+            z = dist["z"]
+            pca = PCA(n_components=3)
+            print(pca.fit(z))
 
-            plt.scatter(mus[sampled], sigmas[sampled], label=classes[label], s=20)
-        plt.legend()
-        plt.savefig("result.png")
+            # total = len(mus)
+            # if total > 500:
+            #     sampled = random.sample(list(range(total)), 500)
+            # else:
+            #     sampled = list(range(total))
+
+            # ax.scatter(z, label=classes[label], s=20)
+        ax.legend()
+        ax.savefig("result.png")
