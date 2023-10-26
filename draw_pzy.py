7a55ed4c35722b164e62bfb14ab192faf8cbc112
923048992@qq.com <923048992@qq.com>
2023-10-26 08:14:48 +0800

    fix

--- draw_pzy.py
+++ draw_pzy.py
@@ -34,6 +34,8 @@ if __name__ == "__main__":
         r_mus = state_dict["r.mu"].to(device)
         r_sigmas = state_dict["r.sigma"].to(device)
         r_C = state_dict["r.C"].to(device)
+
+        print(r_mus, r_sigmas)
         state_dict.pop("r.mu")
         state_dict.pop("r.sigma")
         state_dict.pop("r.C")
@@ -48,8 +50,6 @@ if __name__ == "__main__":
                 images, labels = images.to(device), labels.to(device)
                 z, output, (z_mu, z_sigma) = model(images, return_dist=True)
                 predict = torch.argmax(output, dim=1)
-                r_sigma = r_sigmas[labels]
-                r_mu = r_mus[labels]
                 for i in range(len(predict)):
                     p = predict[i].item()
                     label_dist[p]["z"].append(z[i].cpu().detach().tolist())
@@ -65,13 +65,12 @@ if __name__ == "__main__":
             dist = label_dist[label]
             mus = np.array(dist["mu"])
             sigmas = np.array(dist["sigma"])
-            x_, y_ = z[:, 0], z[:, 1]
-            this_r_mu = r_mu[label].mean().item()
-            this_r_sigma = r_sigma[label].mean().item()
+            r_mu = r_mus[label].mean().item()
+            r_sigma = r_sigmas[label].mean().item()
             plt.clf()
-            plt.scatter(x_, y_, label=classes[label], s=20)
-            plt.axhline(y=this_r_sigma, linestyle="--", label="$r_\mu$")
-            plt.axvline(x=this_r_mu, linestyle="--", label="$r_\sigma$")
+            plt.scatter(mus, sigmas, label=classes[label], s=20)
+            plt.axhline(y=r_sigma, linestyle="--", label="$r_\mu$")
+            plt.axvline(x=r_mu, linestyle="--", label="$r_\sigma$")
             plt.xlabel("$\mu$")
             plt.ylabel("$\sigma$")
             cls = classes[label]
